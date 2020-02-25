bf7f211f7516d7cfa16a46096a04bd2a7726eb93
aacostad-everis <30773179+aacostad-everis@users.noreply.github.com>
2020-02-25 09:55:19 +0100

    Update login_page.php

--- login_page.php
+++ login_page.php
@@ -1,45 +1,31 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <!--<meta name="salesforce-community" content="https://<?php echo getenv('SALESFORCE_COMMUNITY_URL');?>">
-	<meta name="salesforce-mode" content="<?php echo getenv('SALESFORCE_MODE');?>-callback">
-	<meta name="salesforce-save-access-token" content="true">
-	<meta name="salesforce-allowed-domains" content="<?php echo getenv('SALESFORCE_HEROKUAPP_URL');?>">
-	<script src="https://<?php echo getenv('SALESFORCE_COMMUNITY_URL');?>/servlet/servlet.loginwidgetcontroller?type=javascript_widget" async defer></script>-->
-
-</head> 
-<body>
-	<?php
-		function myFunction() {
-			define("USERNAME", "tgodoyr.damm@everis.com");
-			define("PASSWORD", "Tg28122019");
-			define("SECURITY_TOKEN", "edQ9hwiWyF4rtuPciKEwnAOoZ");
-			
-			echo "Before importing";
-			
-			require_once ('soapclient/SforceEnterpriseClient.php');
-			
-			echo "Before creating instance";
-			
-			$mySforceConnection = new SforceEnterpriseClient();
-			$mySforceConnection->createConnection("enterprise.wsdl.xml");
-			
-			echo "Before login";
-			
-			$mySforceConnection->login(USERNAME, PASSWORD.SECURITY_TOKEN);
-			
-			echo "after login";
-			
-			echo $mySforceConnection->sessionId;
-		}
+<?php
+	function myFunction() {
+		define("USERNAME", "tgodoyr.damm@everis.com");
+		define("PASSWORD", "Tg28122019");
+		define("SECURITY_TOKEN", "edQ9hwiWyF4rtuPciKEwnAOoZ");
+		
+		echo "Before importing";
+		
+		require_once ('soapclient/SforceEnterpriseClient.php');
+		
+		echo "Before creating instance";
+		
+		$mySforceConnection = new SforceEnterpriseClient();
+		$mySforceConnection->createConnection("enterprise.wsdl.xml");
 	
-		ini_set('display_errors', 1);
-		ini_set('display_startup_errors', 1);
-		error_reporting(E_ALL);
+		echo "Before login";
+		
+		$mySforceConnection->login(USERNAME, PASSWORD.SECURITY_TOKEN);
+		
+		echo "after login";
+		
+		echo $mySforceConnection->sessionId;
+	}
 
-		myFunction();
-	?>
+	ini_set('display_errors', 1);
+	ini_set('display_startup_errors', 1);
+	error_reporting(E_ALL);
+	
+	myFunction();
+?>
 
-<button onclick="myFunction()">Get login info</button>
-</body>    
-</html>
